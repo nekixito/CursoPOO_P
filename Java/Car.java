9a73a0d455ec923cd6d8018590529eb157bf72b2
Miguel Nequiz <nekixito@gmail.com>
2021-10-20 12:00:55 -0500

    Creando y mostrando objetos en Java y en Python

--- Java/Car.java
+++ Java/Car.java
@@ -2,5 +2,11 @@ class Car {
     Integer id;
     String license;
     String driver;
-    Integer passenger;    
+    Integer passenger;
+    
+    void printDataCar(){
+        System.out.println("Car License: " + license);
+        System.out.println("Car Driver: " + driver);
+        System.out.println("Car Passenger: " + passenger);
+    }
 }
